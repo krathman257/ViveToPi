c706769f4cc6c5721f342f6ad1e4435d3bb74e62
krathman257 <krathman257@gmail.com>
2022-11-27 23:01:07 -0500

    Added build directory

---
+++ build/CMakeFiles/viveToPi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/viveToPi.dir/viveToPi.cpp.o"
+  "viveToPi"
+  "viveToPi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/viveToPi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
